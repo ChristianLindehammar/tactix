d7d6e5b3ac38344bfb2622e5a775b547074bf1eb
Christian Lindehammar <christian.lindehammar@sectra.com>
2025-01-31 11:20:16 +0100

    Update FloorballSvg component paths for consistency and styling

--- components/ui/FloorballSvg.tsx
+++ components/ui/FloorballSvg.tsx
@@ -15,29 +15,31 @@ const SvgComponent = (props: SvgProps) => (
       x={2.505}
       y={905.5}
       fill="#0097B2"
+      stroke="#fff"
+      strokeWidth={5}
       rx={37.5}
       transform="rotate(-90 2.505 905.5)"
     />
     <Path
       stroke="#fff"
       strokeWidth={5}
-      d="M406 841.5h23M417.5 830v23M55 66.5h23M66.5 55v23M55 841.5h23M66.5 830v23M406 66.5h23M417.5 55v23M5 453h474.009M175.5 859.5v-120h133v120z"
+      d="M430 841.5h23M441.5 830v23M31 66.5h23M42.5 55v23M31 841.5h23M42.5 830v23M430 66.5h23M441.5 55v23M5 453h474.009M183.5 48.5v92h118v-92z"
     />
     <Path
       stroke="#fff"
       strokeWidth={5}
-      d="M210.5 822.5v-45h63v45zM264.5 822v22M219.5 822v22M175.5 48.5v120h133v-120z"
+      d="M214.5 67.918v20.427h56V67.918zM223.5 67.418V58M262.5 67.418V58M183.5 859.5v-92h118v92z"
     />
     <Path
       stroke="#fff"
       strokeWidth={5}
-      d="M210.5 85.5v45h63v-45zM264.5 86V64M219.5 86V64"
+      d="M214.5 840.083v-20.427h56v20.427zM223.5 840.583V850M262.5 840.583V850"
     />
     <Path
       fill="#fff"
       d="M242 444a9 9 0 0 1 9 9 9 9 0 0 1-9 9 9 9 0 0 1-9-9 9 9 0 0 1 9-9Z"
     />
-    <Path stroke="#fff" strokeWidth={5} d="M420.5 442v23M68.5 441v23" />
+    <Path stroke="#fff" strokeWidth={5} d="M441.5 442v23M42.5 442v23" />
   </Svg>
 )
 const FloorballSvg = memo(SvgComponent)
